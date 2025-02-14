5d44a66688c9b4e133eaa75930b872926e08143d
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-02-14 23:13:35 +1300

    Add logo component

---
+++ components/general/Logo.tsx
@@ -0,0 +1,16 @@
+import { FolderKanban } from 'lucide-react'
+
+type LogoProps = {
+  size?: number
+  className?: string
+}
+export function Logo({ size = 40, className }: LogoProps) {
+  return (
+    <div className={`flex items-center justify-center ${className}`}>
+      <FolderKanban size={size} className='text-primary' />
+      <h1 className={`font-bold ${className}`}>
+        Job <span className='text-primary'>Board</span>
+      </h1>
+    </div>
+  )
+}
