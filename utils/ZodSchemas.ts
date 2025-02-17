0318838aa8b4f909feccb56ab8c52033c908bff4
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-02-18 08:17:50 +1300

    Create JobSeeker zod schema

--- utils/ZodSchemas.ts
+++ utils/ZodSchemas.ts
@@ -10,3 +10,9 @@ export const CompanySchema = z.object({
   website: z.string().url('Please provide a valid URL'),
   xAccount: z.string().optional(),
 })
+
+export const JobSeekerSchema = z.object({
+  name: z.string().min(2, 'Name must be at least 2 characters long'),
+  about: z.string().min(10, 'Please provide some information about yourself'),
+  resume: z.string().min(1, 'Please upload your resume'),
+})
