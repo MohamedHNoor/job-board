5a3bbee8829c39e004e5b29a507f09bcae88d8bd
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-02-18 08:20:20 +1300

    Check if user completed onboarding

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,6 +1,28 @@
 import { OnboardingForm } from '@/components/forms/onboarding/OnboardingForm'
+import { prisma } from '@/utils/db'
+import { requireUser } from '@/utils/requireUser'
+import { redirect } from 'next/navigation'
 
-export default function OnboardingPage() {
+async function checkIfUserCompletedOnboarding(userId: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      onboardingCompleted: true,
+    },
+  })
+  if (user?.onboardingCompleted === true) {
+    return redirect('/')
+  }
+
+  return user
+}
+
+export default async function OnboardingPage() {
+  const session = await requireUser()
+  await checkIfUserCompletedOnboarding(session.id as string)
+
   return (
     <div className='min-h-screen w-screen flex flex-col items-center justify-center py-10'>
       <OnboardingForm />
