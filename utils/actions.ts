21debf74330d56032365b5ab2515c477412ee18c
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-02-19 12:06:28 +1300

    Add jobSeeker action function

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,7 +2,7 @@
 
 import { requireUser } from './requireUser'
 import { z } from 'zod'
-import { CompanySchema } from './ZodSchemas'
+import { CompanySchema, JobSeekerSchema } from './ZodSchemas'
 import { prisma } from './db'
 import { redirect } from 'next/navigation'
 
@@ -19,6 +19,28 @@ export async function createCompany(data: z.infer<typeof CompanySchema>) {
       onboardingCompleted: true,
       userType: 'COMPANY',
       Company: {
+        create: {
+          ...validateData,
+        },
+      },
+    },
+  })
+  return redirect('/')
+}
+
+export async function createJobSeeker(data: z.infer<typeof JobSeekerSchema>) {
+  const session = await requireUser()
+
+  const validateData = JobSeekerSchema.parse(data)
+
+  await prisma.user.update({
+    where: {
+      id: session.id,
+    },
+    data: {
+      onboardingCompleted: true,
+      userType: 'JOB_SEEKER',
+      JobSeeker: {
         create: {
           ...validateData,
         },
