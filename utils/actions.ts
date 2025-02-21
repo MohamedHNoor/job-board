c07babe8a3ce4706de7e254bfc084bd22b0c8ad1
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-02-21 17:17:35 +1300

    Add arcjet

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,9 +5,31 @@ import { z } from 'zod'
 import { CompanySchema, JobSeekerSchema } from './ZodSchemas'
 import { prisma } from './db'
 import { redirect } from 'next/navigation'
+import arcjet, { detectBot, shield } from './arcjet'
+import { request } from '@arcjet/next'
+
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: 'LIVE',
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: 'LIVE',
+      allow: [],
+    })
+  )
 
 export async function createCompany(data: z.infer<typeof CompanySchema>) {
   const session = await requireUser()
+
+  const req = await request()
+  const decision = await aj.protect(req)
+
+  if (decision.isDenied()) {
+    throw new Error('Access denied')
+  }
 
   const validateData = CompanySchema.parse(data)
 
@@ -30,6 +52,13 @@ export async function createCompany(data: z.infer<typeof CompanySchema>) {
 
 export async function createJobSeeker(data: z.infer<typeof JobSeekerSchema>) {
   const session = await requireUser()
+
+  const req = await request()
+  const decision = await aj.protect(req)
+
+  if (decision.isDenied()) {
+    throw new Error('Access denied')
+  }
 
   const validateData = JobSeekerSchema.parse(data)
 
