a6d8f94ceee10cdebe80613a497f2d346c01f056
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-03-04 16:23:12 +1300

    Update createJobForm component

--- components/forms/CreateJobForm.tsx
+++ components/forms/CreateJobForm.tsx
@@ -1,3 +1,153 @@
+'use client'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { JobSchema } from '@/utils/ZodSchemas'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { Input } from '../ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
+import { SelectLabel } from '@radix-ui/react-select'
+import { countryList } from '@/utils/countriesList'
+
 export function CreateJobForm() {
-  return <div>Job Form</div>
+  const form = useForm<z.infer<typeof JobSchema>>({
+    resolver: zodResolver(JobSchema),
+    defaultValues: {
+      jobTitle: '',
+      employmentType: '',
+      location: '',
+      salaryFrom: 0,
+      salaryTo: 0,
+      jobDescription: '',
+      listingDuration: 30,
+      benefits: [],
+      companyName: '',
+      companyLocation: '',
+      companyLogo: '',
+      companyWebsite: '',
+      companyXAccount: '',
+      companyDescription: '',
+      companyAbout: '',
+    },
+  })
+  return (
+    <Form {...form}>
+      <form className='col-span-1 lg:col-span-2 flex flex-col gap-8'>
+        <Card>
+          <CardHeader>
+            <CardTitle>Job Information</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+              {/* job title */}
+              <FormField
+                control={form.control}
+                name='jobTitle'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Job Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Job title' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* employment type */}
+              <FormField
+                control={form.control}
+                name='employmentType'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Employment Type</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder='Select employment type' />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Employment Type</SelectLabel>
+                          <SelectItem value='full-time'>Full Time</SelectItem>
+                          <SelectItem value='part-time'>Part Time</SelectItem>
+                          <SelectItem value='contract'>Contract</SelectItem>
+                          <SelectItem value='internship'>Internship</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+              {/* location */}
+              <FormField
+                control={form.control}
+                name='location'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Job Location</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder='Select location' />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Worldwide</SelectLabel>
+                          <SelectItem value='worldwide'>
+                            <span>🌍</span>
+                            <span className='pl-2'>Worldwide / Remote</span>
+                          </SelectItem>
+                        </SelectGroup>
+                        <SelectGroup>
+                          <SelectLabel>Location</SelectLabel>
+                          {countryList.map((country) => (
+                            <SelectItem key={country.code} value={country.name}>
+                              <span>{country.flagEmoji}</span>
+                              <span className='pl-2'>{country.name}</span>
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* salary */}
+              <FormItem>
+                <FormLabel>Salary Range</FormLabel>
+                <FormControl></FormControl>
+              </FormItem>
+            </div>
+          </CardContent>
+        </Card>
+      </form>
+    </Form>
+  )
 }
